e0ace7513b2c0330c1080b6313e41757a149e862
Rune Holmdal <runehol@viken.no>
2022-01-25 10:09:47 +0100

    1. steg på veien
    :-)

---
+++ 3daaKalk/3daaKalk/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace _3daaKalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDisplay = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnPluss = new System.Windows.Forms.Button();
+            this.btnErlik = new System.Windows.Forms.Button();
+            this.btSlett = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbDisplay
+            // 
+            this.tbDisplay.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbDisplay.Location = new System.Drawing.Point(304, 44);
+            this.tbDisplay.Name = "tbDisplay";
+            this.tbDisplay.Size = new System.Drawing.Size(197, 33);
+            this.tbDisplay.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(372, 83);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(60, 60);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(306, 83);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(60, 60);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btnPluss
+            // 
+            this.btnPluss.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPluss.Location = new System.Drawing.Point(441, 83);
+            this.btnPluss.Name = "btnPluss";
+            this.btnPluss.Size = new System.Drawing.Size(60, 60);
+            this.btnPluss.TabIndex = 3;
+            this.btnPluss.Text = "+";
+            this.btnPluss.UseVisualStyleBackColor = true;
+            this.btnPluss.Click += new System.EventHandler(this.btnPluss_Click);
+            // 
+            // btnErlik
+            // 
+            this.btnErlik.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnErlik.Location = new System.Drawing.Point(441, 215);
+            this.btnErlik.Name = "btnErlik";
+            this.btnErlik.Size = new System.Drawing.Size(60, 60);
+            this.btnErlik.TabIndex = 4;
+            this.btnErlik.Text = "=";
+            this.btnErlik.UseVisualStyleBackColor = true;
+            this.btnErlik.Click += new System.EventHandler(this.btnErlik_Click);
+            // 
+            // btSlett
+            // 
+            this.btSlett.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btSlett.Location = new System.Drawing.Point(507, 83);
+            this.btSlett.Name = "btSlett";
+            this.btSlett.Size = new System.Drawing.Size(60, 60);
+            this.btSlett.TabIndex = 5;
+            this.btSlett.Text = "C";
+            this.btSlett.UseVisualStyleBackColor = true;
+            this.btSlett.Click += new System.EventHandler(this.btSlett_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMinus.Location = new System.Drawing.Point(441, 149);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(60, 60);
+            this.btnMinus.TabIndex = 6;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btSlett);
+            this.Controls.Add(this.btnErlik);
+            this.Controls.Add(this.btnPluss);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tbDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDisplay;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnPluss;
+        private System.Windows.Forms.Button btnErlik;
+        private System.Windows.Forms.Button btSlett;
+        private System.Windows.Forms.Button btnMinus;
+    }
+}
+
---
+++ 3daaKalk/3daaKalk/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3daaKalk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "2";
+        }
+
+        string tall1 = "";
+        string tall2 = "";
+        string regneOp = ""; //"+", "-" ("pluss", "minus")
+        private void btnPluss_Click(object sender, EventArgs e)
+        {
+            tall1 = tbDisplay.Text;
+            tbDisplay.Text = "";
+            regneOp = btnPluss.Text;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            tall1 = tbDisplay.Text;
+            tbDisplay.Text = "";
+            regneOp = btnMinus.Text;
+        }
+
+
+        private void btnErlik_Click(object sender, EventArgs e)
+        {
+            tall2 = tbDisplay.Text;
+            int t1 = Convert.ToInt32(tall1);
+            int t2 = Convert.ToInt32(tall2);
+            int r = 0;
+            // - --> int s = t1 - t2;
+            if (regneOp == "+")
+                r = t1 + t2;
+            else if (regneOp == "-")
+                r = t1 - t2;
+            string resultat = Convert.ToString(r);
+            tbDisplay.Text = resultat;
+        }
+
+        private void btSlett_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = "";
+            tall1 = "";
+            tall2 = "";
+        }
+
+    }
+}
