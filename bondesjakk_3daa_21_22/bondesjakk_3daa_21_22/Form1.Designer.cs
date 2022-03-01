a48a6414ebb1a013a1da641763389a7260d1bb03
Rune Holmdal <runehol@viken.no>
2022-03-01 08:29:03 +0100

    Så langt
    :-)

---
+++ bondesjakk_3daa_21_22/bondesjakk_3daa_21_22/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace bondesjakk_3daa_21_22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0_0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1_0 = new System.Windows.Forms.Button();
+            this.btn2_0 = new System.Windows.Forms.Button();
+            this.btn2_1 = new System.Windows.Forms.Button();
+            this.btn1_1 = new System.Windows.Forms.Button();
+            this.btn0_1 = new System.Windows.Forms.Button();
+            this.btn2_2 = new System.Windows.Forms.Button();
+            this.btn1_2 = new System.Windows.Forms.Button();
+            this.btn0_2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn0_0
+            // 
+            this.btn0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0_0.Location = new System.Drawing.Point(233, 263);
+            this.btn0_0.Name = "btn0_0";
+            this.btn0_0.Size = new System.Drawing.Size(70, 65);
+            this.btn0_0.TabIndex = 0;
+            this.btn0_0.UseVisualStyleBackColor = true;
+            this.btn0_0.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(306, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Spiller 1";
+            // 
+            // btn1_0
+            // 
+            this.btn1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1_0.Location = new System.Drawing.Point(309, 263);
+            this.btn1_0.Name = "btn1_0";
+            this.btn1_0.Size = new System.Drawing.Size(70, 65);
+            this.btn1_0.TabIndex = 2;
+            this.btn1_0.UseVisualStyleBackColor = true;
+            this.btn1_0.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn2_0
+            // 
+            this.btn2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2_0.Location = new System.Drawing.Point(385, 263);
+            this.btn2_0.Name = "btn2_0";
+            this.btn2_0.Size = new System.Drawing.Size(70, 65);
+            this.btn2_0.TabIndex = 3;
+            this.btn2_0.UseVisualStyleBackColor = true;
+            this.btn2_0.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn2_1
+            // 
+            this.btn2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2_1.Location = new System.Drawing.Point(385, 192);
+            this.btn2_1.Name = "btn2_1";
+            this.btn2_1.Size = new System.Drawing.Size(70, 65);
+            this.btn2_1.TabIndex = 6;
+            this.btn2_1.UseVisualStyleBackColor = true;
+            this.btn2_1.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn1_1
+            // 
+            this.btn1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1_1.Location = new System.Drawing.Point(309, 192);
+            this.btn1_1.Name = "btn1_1";
+            this.btn1_1.Size = new System.Drawing.Size(70, 65);
+            this.btn1_1.TabIndex = 5;
+            this.btn1_1.UseVisualStyleBackColor = true;
+            this.btn1_1.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn0_1
+            // 
+            this.btn0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0_1.Location = new System.Drawing.Point(233, 192);
+            this.btn0_1.Name = "btn0_1";
+            this.btn0_1.Size = new System.Drawing.Size(70, 65);
+            this.btn0_1.TabIndex = 4;
+            this.btn0_1.UseVisualStyleBackColor = true;
+            this.btn0_1.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn2_2
+            // 
+            this.btn2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2_2.Location = new System.Drawing.Point(385, 121);
+            this.btn2_2.Name = "btn2_2";
+            this.btn2_2.Size = new System.Drawing.Size(70, 65);
+            this.btn2_2.TabIndex = 9;
+            this.btn2_2.UseVisualStyleBackColor = true;
+            this.btn2_2.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn1_2
+            // 
+            this.btn1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1_2.Location = new System.Drawing.Point(309, 121);
+            this.btn1_2.Name = "btn1_2";
+            this.btn1_2.Size = new System.Drawing.Size(70, 65);
+            this.btn1_2.TabIndex = 8;
+            this.btn1_2.UseVisualStyleBackColor = true;
+            this.btn1_2.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // btn0_2
+            // 
+            this.btn0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0_2.Location = new System.Drawing.Point(233, 121);
+            this.btn0_2.Name = "btn0_2";
+            this.btn0_2.Size = new System.Drawing.Size(70, 65);
+            this.btn0_2.TabIndex = 7;
+            this.btn0_2.UseVisualStyleBackColor = true;
+            this.btn0_2.Click += new System.EventHandler(this.btnSpill_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 471);
+            this.Controls.Add(this.btn2_2);
+            this.Controls.Add(this.btn1_2);
+            this.Controls.Add(this.btn0_2);
+            this.Controls.Add(this.btn2_1);
+            this.Controls.Add(this.btn1_1);
+            this.Controls.Add(this.btn0_1);
+            this.Controls.Add(this.btn2_0);
+            this.Controls.Add(this.btn1_0);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn0_0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0_0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1_0;
+        private System.Windows.Forms.Button btn2_0;
+        private System.Windows.Forms.Button btn2_1;
+        private System.Windows.Forms.Button btn1_1;
+        private System.Windows.Forms.Button btn0_1;
+        private System.Windows.Forms.Button btn2_2;
+        private System.Windows.Forms.Button btn1_2;
+        private System.Windows.Forms.Button btn0_2;
+    }
+}
+
---
+++ bondesjakk_3daa_21_22/bondesjakk_3daa_21_22/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bondesjakk_3daa_21_22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool spiller = true; // true = spiller 1 og false = spiller 2
+        private void btn0_0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSpill_click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (b.Text == "")
+            {
+                if (spiller)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+                spiller = !spiller;
+                //spiller != spiller; alternativ måte til linjen over
+                if (spiller)
+                    label1.Text = "Spiller 1";
+                else
+                    label1.Text = "Spiller 2";
+            }
+        }
+    }
+}
